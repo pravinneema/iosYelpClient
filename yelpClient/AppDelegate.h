bbf481cdf9a85c5bb305874bced166174cf5e9c4
Pravin Neema <pneema@yahoo-inc.com>
2014-06-18 22:27:45 -0700

    Initial Commit

---
+++ yelpClient/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  yelpClient
+//
+//  Created by Pravin Neema on 6/18/14.
+//  Copyright (c) 2014 Pravin Neema. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ yelpClient/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  yelpClient
+//
+//  Created by Pravin Neema on 6/18/14.
+//  Copyright (c) 2014 Pravin Neema. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
